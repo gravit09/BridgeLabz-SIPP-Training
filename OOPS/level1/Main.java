a0202b3c720571f0d448129470136b68d0b23995
Gravit09 <garvitchoudhary09@gmail.com>
2025-06-24 09:16:36 +0530

    OOPS level1 solved

---
+++ OOPS/level1/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] foodItems1 = {"Pizza", "Pasta", "Burger"};
+        String[] foodItems2 = {"Sushi", "Ramen", "Tempura"};
+        
+        Restaurant restaurant1 = new Restaurant("Italian Delight", "Downtown", foodItems1);
+        Restaurant restaurant2 = new Restaurant("Tokyo Treats", "Uptown", foodItems2);
+        
+        System.out.println("=== Restaurant 1 ===");
+        restaurant1.displayDetails();
+        System.out.println("\n=== Restaurant 2 ===");
+        restaurant2.displayDetails();
+        
+        System.out.println("\nChecking Food Availability:");
+        System.out.println("Is Pasta available in Italian Delight? " + 
+            restaurant1.isFoodAvailable("Pasta"));
+        System.out.println("Is Sushi available in Italian Delight? " + 
+            restaurant1.isFoodAvailable("Sushi"));
+    }
+} 
