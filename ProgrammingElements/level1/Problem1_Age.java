b8aff7c3f2ed8c33f427326d5337d41b7bc6450f
Gravit09 <garvitchoudhary09@gmail.com>
2025-09-09 19:53:51 +0530

    Streams and fi done

--- ProgrammingElements/level1/Problem1_Age.java
+++
@@ -1,8 +0,0 @@
-package ProgrammingElements.level1;
-
-public class Problem1_Age {
-    public static void main(String[] args) {
-        int birthYear = 2000;
-        System.out.println("Harry's age in 2024 is " + (2024-2000));
-    }
-} 
